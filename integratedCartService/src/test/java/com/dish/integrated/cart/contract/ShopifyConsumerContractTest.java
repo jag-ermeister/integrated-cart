a575383bdbae9b941cf23f7aad9e5e87e8792b63
Groff, Laird <laird.groff@dish.com>
2020-02-20 20:18:27 +0000

    changed var

--- integratedCartService/src/test/java/com/dish/integrated/cart/contract/ShopifyConsumerContractTest.java
+++ integratedCartService/src/test/java/com/dish/integrated/cart/contract/ShopifyConsumerContractTest.java
@@ -27,57 +27,57 @@ import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 public class ShopifyConsumerContractTest {
 
-//    static final String SHOPIFY_ORDERS_API = "/admin/api/2020-01/orders.json";
-//    static final String SHOPIFY_ORDER_REQUEST_FILE = "shopify_order_request.json";
-//    static final String SHOPIFY_ORDER_RESPONSE_FILE = "shopify_order_response.json";
-//
-//    JsonParser parser = new JsonParser();
-//
-//    @Rule
-//    public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("shopify_orders_provider", "localhost", 8090, this);
-//
-//    @Pact(consumer = "shopify_orders_consumer", provider="shopify_orders_provider")
-//    public RequestResponsePact createPact(PactDslWithProvider builder) {
-//        return builder
-//                .given("test POST")
-//                .uponReceiving("POST REQUEST")
-//                .path(SHOPIFY_ORDERS_API)
-//                .body(getResourceContents(SHOPIFY_ORDER_REQUEST_FILE))
-//                .method("POST")
-//                .willRespondWith()
-//                .status(201)
-//                .headers(ImmutableMap.of(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-//                .body(getResourceContents(SHOPIFY_ORDER_RESPONSE_FILE))
-//                .toPact();
-//    }
-//
-//    @Test
-//    @PactVerification
-//    public void submitOrder_shouldReturn201WithProperHeaderAndBody() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//
-//        ResponseEntity<String> response = new RestTemplate().postForEntity(
-//            mockProvider.getUrl() + SHOPIFY_ORDERS_API,
-//                new HttpEntity<>(getResourceContents(SHOPIFY_ORDER_REQUEST_FILE), headers),
-//                String.class
-//        );
-//
-//        assertThat(response.getStatusCode().value()).isEqualTo(201);
-//        assertThat(response.getHeaders().get(CONTENT_TYPE).contains(MediaType.APPLICATION_JSON_VALUE)).isTrue();
-//        assertEquals(getJsonFromFile(SHOPIFY_ORDER_RESPONSE_FILE), parser.parse(response.getBody()));
-//    }
-//
-//    private JsonElement getJsonFromFile(String filename) {
-//        return parser.parse(getResourceContents(filename));
-//    }
-//
-//    private String getResourceContents(String resourceFilename) {
-//        try {
-//            Path resourceFilePath = Paths.get("src", "test", "resources", resourceFilename);
-//            return Files.readString(resourceFilePath);
-//        } catch (IOException e) {
-//            throw new RuntimeException("Failed to read from resource file: " + resourceFilename);
-//        }
-//    }
+    static final String SHOPIFY_ORDERS_API = "/admin/api/2020-01/orders.json";
+    static final String SHOPIFY_ORDER_REQUEST_FILE = "shopify_order_request.json";
+    static final String SHOPIFY_ORDER_RESPONSE_FILE = "shopify_order_response.json";
+
+    JsonParser parser = new JsonParser();
+
+    @Rule
+    public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2("shopify_orders_provider", "localhost", 8090, this);
+
+    @Pact(consumer = "shopify_orders_consumer", provider="shopify_orders_provider")
+    public RequestResponsePact createPact(PactDslWithProvider builder) {
+        return builder
+                .given("test POST")
+                .uponReceiving("POST REQUEST")
+                .path(SHOPIFY_ORDERS_API)
+                .body(getResourceContents(SHOPIFY_ORDER_REQUEST_FILE))
+                .method("POST")
+                .willRespondWith()
+                .status(201)
+                .headers(ImmutableMap.of(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .body(getResourceContents(SHOPIFY_ORDER_RESPONSE_FILE))
+                .toPact();
+    }
+
+    @Test
+    @PactVerification
+    public void submitOrder_shouldReturn201WithProperHeaderAndBody() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        ResponseEntity<String> response = new RestTemplate().postForEntity(
+            mockProvider.getUrl() + SHOPIFY_ORDERS_API,
+                new HttpEntity<>(getResourceContents(SHOPIFY_ORDER_REQUEST_FILE), headers),
+                String.class
+        );
+
+        assertThat(response.getStatusCode().value()).isEqualTo(201);
+        assertThat(response.getHeaders().get(CONTENT_TYPE).contains(MediaType.APPLICATION_JSON_VALUE)).isTrue();
+        assertEquals(getJsonFromFile(SHOPIFY_ORDER_RESPONSE_FILE), parser.parse(response.getBody()));
+    }
+
+    private JsonElement getJsonFromFile(String filename) {
+        return parser.parse(getResourceContents(filename));
+    }
+
+    private String getResourceContents(String resourceFilename) {
+        try {
+            Path resourceFilePath = Paths.get("src", "test", "resources", resourceFilename);
+            return Files.readString(resourceFilePath);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read from resource file: " + resourceFilename);
+        }
+    }
 }
